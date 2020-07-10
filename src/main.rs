4ea298ad9b441183cf3bb17852e800f3b8b511e6
psato <forhighlow@gmail.com>
2020-07-10 14:56:20 +0900

    ディレクトリの前に├──を表示させる

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
-use std::path::PathBuf;
+use std::fs;
+use std::io;
+use std::path::{Path, PathBuf};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 struct Opt {
-    #[structopt(default_value = ".",parse(from_os_str))]
+    #[structopt(default_value = ".", parse(from_os_str))]
     path: PathBuf,
 }
+
+// fn visit_dirs(dir: &Path) -> io::Result<()> {
+//     let mut paths: Vec<_> = fs::read_dir(dir)?
+//         .map(|entry| entry.unwrap().path())
+//         .collect();
+// }
 
 fn main() {
     let opt = Opt::from_args();
     println!("opt = {:?}", opt);
     for entry in opt.path.read_dir().expect("read_dir call failed") {
         if let Ok(entry) = entry {
-            println!("{:?}", entry.path());
+            println!("{}├── {:?}", String::from(""), entry.path());
         }
     }
 }
