3c05ebc09003a41bedd81d370db0e65bf1bc7a3f
psato <forhighlow@gmail.com>
2020-07-14 11:32:34 +0900

    ツリーにはなったけど、ファイル名じゃなくてフルパスになっている

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use std::error::Error;
 use std::fs;
-use std::io;
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
 
@@ -11,18 +11,28 @@ use structopt::StructOpt;
 //     pub const BLANK: char = '\u{00A0}';
 // }
 static LAST_FILE: &str = "└───";
-static CROSS: &str = "├──";
+static CROSS: &str = "├───";
+static BLANK: &str = "\u{00A0}\u{00A0}\u{00A0}\u{00A0}";
+static VERT: &str = "│   ";
 
 #[derive(StructOpt, Debug)]
 struct Opt {
     #[structopt(default_value = ".", parse(from_os_str))]
     path: PathBuf,
+
+    #[structopt(short = "L", default_value = "2")]
+    level: usize,
 }
-fn run(dir: &Path) {
-    visit_dirs(&dir, String::from(""))
+fn run(dir: &Path, level: usize) {
+    let default_depth = 0;
+    let default_prefix = String::from("");
+    visit_dirs(&dir, default_depth, level, default_prefix);
 }
 
-fn visit_dirs(dir: &Path, prefix: String) {
+fn visit_dirs(dir: &Path, depth: usize, level: usize, prefix: String) {
+    // if depth == level {
+    //     return Ok(());
+    // }
     let mut paths: Vec<_> = fs::read_dir(&dir)
         .unwrap()
         .map(|entry| entry.unwrap())
@@ -35,9 +45,19 @@ fn visit_dirs(dir: &Path, prefix: String) {
         for (index, entry) in paths.iter().enumerate() {
             let path = entry.path();
             if index == paths.len() - 1 {
-                println!("{}{}{:?}", prefix, &LAST_FILE, &path.display())
+                println!("{}{}{}", prefix, &LAST_FILE, &path.display());
+                if path.is_dir() {
+                    let depth = depth + 1;
+                    let prefix = prefix.clone() + &BLANK;
+                    visit_dirs(&path, depth, level, prefix);
+                }
             } else {
-                println!("{}{}{}", prefix, &CROSS, &path.display())
+                println!("{}{}{}", prefix, &CROSS, &path.display());
+                if path.is_dir() {
+                    let depth = depth + 1;
+                    let prefix_new = prefix.clone() + &VERT;
+                    visit_dirs(&path, depth, level, prefix_new);
+                }
             }
         }
     }
@@ -46,5 +66,5 @@ fn visit_dirs(dir: &Path, prefix: String) {
 fn main() {
     let opt = Opt::from_args();
     // println!("opt = {:?}", opt);
-    run(&opt.path)
+    run(&opt.path, opt.level)
 }
